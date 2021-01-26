e1f3bf8e2a5dc6c226b90a383b0174a1c0ed8734
刘伟 <liuwei001@126.com>
2021-01-26 13:19:36 +0800

    0126

--- pages/demo06/demo06.js
+++ pages/demo06/demo06.js
@@ -1,74 +1,54 @@
-Page({
-  data: {
-    delBtnWidth:160,
-    data: [{ content: "1", right: 0 }, { content: "2", right: 0 }, { content: "3", right: 0 }, { content: "4", right: 0 }, { content: "5", right: 0 }, { content: "6", right: 0 }, { content: "7", right: 0 }, { content: "8", right: 0 }, { content: "9", right: 0 }, { content: "10",  right: 0 }],
-    isScroll:true,
-    windowHeight:0,
-  },
-  onLoad: function (options) {
-    var that = this;
-    wx.getSystemInfo({
-      success: function (res) {
-        that.setData({
-          windowHeight: res.windowHeight
-        });
+const app = getApp();
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    tabbar: {
+      type: Object,
+      value: {
+        "backgroundColor": "#ffffff",
+        "color": "#979795",
+        "selectedColor": "#1c1c1b",
+        "list": [
+          {
+            "pagePath": "/pages/teacher/sign_home",
+            "text": "首页",
+            "iconPath": "../../images/home1.png",
+            "selectedIconPath": "../../images/home2.png"
+          },
+          {
+            "pagePath": "/pages/teacher/sign_launch",
+            "isSpecial": true,
+            "text": "发布",
+            "iconPath": "../../images/publish.png",
+            "selectedIconPath": "../../images/publish.png",
+          },
+          {
+            "pagePath": "/pages/teacher/sign_my",
+            "text": "我的",
+            "iconPath": "../../images/my1.png",
+            "selectedIconPath": "../../images/my2.png"
+          }
+        ]
       }
-    });
-  },
-  drawStart: function (e) {
-    // console.log("drawStart");  
-    var touch = e.touches[0]
-
-    for(var index in this.data.data) {
-      var item = this.data.data[index]
-      item.right = 0
-    }
-    this.setData({
-      data: this.data.data,
-      startX: touch.clientX,
-    })
-
-  },
-  drawMove: function (e) {
-    var touch = e.touches[0]
-    var item = this.data.data[e.currentTarget.dataset.index]
-    var disX = this.data.startX - touch.clientX
-    
-    if (disX >= 20) {
-      if (disX > this.data.delBtnWidth) {
-        disX = this.data.delBtnWidth
-      }
-      item.right = disX
-      this.setData({
-        isScroll: false,
-        data: this.data.data
-      })
-    } else {
-      item.right = 0
-      this.setData({
-        isScroll: true,
-        data: this.data.data
-      })
-    }
-  },  
-  drawEnd: function (e) {
-    var item = this.data.data[e.currentTarget.dataset.index]
-    if (item.right >= this.data.delBtnWidth/2) {
-      item.right = this.data.delBtnWidth
-      this.setData({
-        isScroll: true,
-        data: this.data.data,
-      })
-    } else {
-      item.right = 0
-      this.setData({
-        isScroll: true,
-        data: this.data.data,
-      })
     }
   },
-  
-  delItem: function (e) {
-
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    shareTherelease(e) {
+      console.log(e.currentTarget.dataset.url);
+      wx.navigateTo({
+        url: e.currentTarget.dataset.url,
+      })
+    },
+    onSwitchTab(e){
+      console.log(e.currentTarget.dataset.url)
+      wx.switchTab({
+        url: e.currentTarget.dataset.url,
+      })
+    }
   }
 })
